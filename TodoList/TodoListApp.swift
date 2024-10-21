099fbe38f0de510f2a7a13a2af434d63f96ae3c1
vishal-Kumar08 <vishaldugri35@gmail.com>
2024-10-21 17:01:51 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by aman kumar on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoListTests/TodoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListTests.swift
+//  TodoListTests
+//
+//  Created by aman kumar on 21/10/24.
+//
+
+import Testing
+@testable import TodoList
+
+struct TodoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
